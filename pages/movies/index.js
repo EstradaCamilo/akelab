cf03481c644061c6e2a1f2b5ca6f7b22358492cb
EstradaCamilo <estradasalinasjosecamilo@gmail.com>
2022-03-05 19:14:03 -0500

    Consultar pelicula

--- pages/movies/index.js
+++ pages/movies/index.js
@@ -14,6 +14,9 @@ export default function Multiples() {
   const [movies, setMovies] = useState([]);
   const [genders, setGenders] = useState([]);
   const [baseImg, setBaseImg] = useState("");
+  const [keyword, setKeyword] = useState("");
+
+  const [moviesFiltered, setMoviesFiltered] = useState([]);
 
   useEffect(() => {
     fetch(`${window.location.origin}/api/movies?akelab=123456789`, {
@@ -23,6 +26,7 @@ export default function Multiples() {
       .then((data) => {
         if (data.movies) {
           setMovies(data.movies.results);
+          setMoviesFiltered(data.movies.results);
           setGenders(data.movies.genres);
           setBaseImg(data.movies.images_url);
         } else {
@@ -30,6 +34,18 @@ export default function Multiples() {
         }
       });
   }, []);
+
+  useEffect(() => {
+    if (keyword == "") {
+      setMoviesFiltered(movies);
+    } else {
+      setMoviesFiltered(
+        movies.filter(({ title }) =>
+          title.toLowerCase().includes(keyword.trim().toLowerCase())
+        )
+      );
+    }
+  }, [keyword, movies]);
 
   const getGender = (id) => {
     if (genders.length > 0) {
@@ -47,7 +63,11 @@ export default function Multiples() {
         <h1>Películas</h1>
         <div className={styles.options}>
           <form className={styles.search}>
-            <input type="search" />
+            <input
+              type="search"
+              value={keyword}
+              onChange={(e) => setKeyword(e.target.value)}
+            />
           </form>
           <div className={styles.filters}>
             <Popover className={styles.genders}>
@@ -121,7 +141,7 @@ export default function Multiples() {
           </div>
         </div>
         <div className={styles.collectionMovies}>
-          {movies.map((movie) => (
+          {moviesFiltered.map((movie) => (
             <CardMovie
               key={movie.id}
               movie={movie}
